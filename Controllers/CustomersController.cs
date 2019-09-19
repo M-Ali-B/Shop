0314b6cee7b6f28419eb76d785a655d1eb3390dc
Ali Baba <alibabatorjan001@gmail.com>
2019-09-19 12:39:26 +0500

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+            //var model = new List<Customer>();
+
+            //model.Add(new Customer { Id = 1, Name = "Ali" });
+            //model.Add(new Customer { Id = 2, Name = "Baba" });
+            //model.Add(new Customer { Id = 3, Name = "Wassu" });
+
+            var model = GetCustomers();
+            return View(model);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Ali" },
+                new Customer { Id = 2, Name = "Baba" },
+                new Customer {Id =3,Name = "Wassu"}
+            };
+        }
+    }
+
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var model = new List<Movie>();
+            model.Add(new Movie { Id = 1, Name = "Wall-e" });
+            model.Add(new Movie { Id = 1, Name = "Upgrade" });
+
+            return View(model);
+        }
+    }
+}
